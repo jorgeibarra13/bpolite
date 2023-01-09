c647d7301bc3230fa66d7806446c87379581d121
Lorenc <ijllbo13@gmail.com>
2023-01-08 21:54:13 -0800

    Support other types of inputs

--- extension/content.js
+++ extension/content.js
@@ -1,11 +1,28 @@
 (() => {
   const replaceTextWithProcession = (procession) => {
-    const selection = window.getSelection();
+    const textarea = document.activeElement;
+    const nodeName = document.activeElement.nodeName.toLocaleLowerCase();
 
-    if (selection.rangeCount) {
-      range = selection.getRangeAt(0);
-      range.deleteContents();
-      range.insertNode(document.createTextNode(procession));
+    if (nodeName.includes('input') || nodeName.includes('textarea')) {
+      if(typeof textarea.selectionStart === 'number' && typeof textarea.selectionEnd === 'number') {
+        // All browsers except IE
+        var start = textarea.selectionStart;
+        var end = textarea.selectionEnd;
+
+        var before = textarea.value.slice(0, start);
+        var after = textarea.value.slice(end);
+
+        var text = before + procession + after;
+        textarea.value = text;
+      }
+    } else {
+      const selection = window.getSelection();
+
+      if (selection.rangeCount) {
+        range = selection.getRangeAt(0);
+        range.deleteContents();
+        range.insertNode(document.createTextNode(procession));
+      }
     }
   }
 
