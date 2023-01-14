84aa497a83b8c849de78e623f2d162d62bffc9f9
Lorenc <ijllbo13@gmail.com>
2023-01-13 16:58:23 -0800

    Fix background.js tab retreival and add backend to manifest hosts

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 async function getCurrentTab() {
   let queryOptions = { active: true, lastFocusedWindow: true };
-  let tab = await chrome.tabs.query(queryOptions);
-  return [tab];
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
 }
 
 // global variable should probably be a class property
